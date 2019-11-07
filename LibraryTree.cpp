f9858eac5aeb28a74cbd0d7d7c703108dfc8362b
amicah <a.micah.mcclain@gmail.com>
2019-11-07 12:07:22 -0800

    Create LibraryTree.cpp
    The main file.

---
+++ LibraryTree.cpp
@@ -0,0 +1,16 @@
+/*
+	* Authors:		Barragan, Alejandro
+	*				Lu, Stephanie
+	*				McClain, Micah
+	*
+	* Date: Nov 7, 2019
+	* Description:
+	*
+	*
+	*
+	* For: San Bernardino Valley College - CS 265: Data Structures - Fall 2019
+*/
+
+int main() {
+	// Todo: Write code.
+}
