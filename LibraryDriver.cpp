e053311d1fc3e963e182d1c132b2b6f5d2fe7324
roombas <56892280+roombas@users.noreply.github.com>
2019-12-13 14:29:30 -0800

    Update LibraryDriver.cpp

--- LibraryDriver.cpp
+++ LibraryDriver.cpp
@@ -1,53 +1,8 @@
 #include <cstdlib>
 #include <iostream>
 #include <string>
+#include <sstream>
 #include "Library.h"
-
-void fillTree(LibraryTree<Book>* tree)
-{
-	ifstream myFile;
-	Book book;
-	string title;
-	string author;
-	string line;
-	string word = "";
-	string dL = "\",\"";
-
-	myFile.open("LibraryData2.csv");//("Titles.txt");//
-	getline(myFile, line); // Skip header line
-
-	if (!myFile.good())
-	{
-		cout << "Invalid file\n";
-	}
-	else {
-		getline(myFile, line); // Get 1st line of data
-		while (myFile.good() && !myFile.eof())
-		{
-			if (myFile.good()) {
-				size_t dLPos = line.find(dL, 0);
-				// Parse Title
-				for (size_t i = 3; i < dLPos - 2; ++i) {
-					word += line[i];
-				}
-				title = word;
-				word = "";
-				// Parse Author
-				for (size_t i = dLPos + 5; i < line.size() - 3; ++i) {
-					word += line[i];
-				}
-				author = word;
-				word = "";
-				book.setTitle(title);
-				book.setAuthor(author);
-				(*tree).insertNode(book);
-				getline(myFile, line); // Get next line of data
-			}
-		}
-	}
-
-	myFile.close();
-}
 
 int main()
 {
