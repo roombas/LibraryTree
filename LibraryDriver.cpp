903d7c98dee7f07fbfeafcca12ba32720ee00add
roombas <56892280+roombas@users.noreply.github.com>
2019-12-14 22:37:55 -0800

    Update LibraryDriver.cpp

--- LibraryDriver.cpp
+++ LibraryDriver.cpp
@@ -1,16 +1,23 @@
-#include <cstdlib>
 #include <iostream>
 #include <string>
-#include <sstream>
 #include "Library.h"
+#include "Extra.h"
+
+LibraryTree<Book> treeT;
+LibraryTree<Book> treeA;
 
 int main()
 {
-	LibraryTree<Book> tree;
-	fillTree(&tree);
-	tree.printTreeInOrder();
-	cout << "Tree depth of: " << tree.maxDepth() << endl;
-	cout << "Tree width of: " << tree.getWidth() << endl;
+	fillTree(&treeT, "Book1.csv");
+
+	insertBook(&treeT, "Axiom's End", "Ellis, Lindsay");
+	treeT.searchSpecific("Axiom's End");
+
+	treeT.changeBookStatus("Axiom's End", 0);
+	removeBook(&treeT, "Axiom's End");
+	treeT.searchSpecific("Axiom's End");
+
+	treeT.printTree();
 
 	return 0;
 }
