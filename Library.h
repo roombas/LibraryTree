8fccde28de8a379f6d1880d373ae882be46221e3
roombas <56892280+roombas@users.noreply.github.com>
2019-12-15 04:33:46 -0800

    Update Library.h

--- Library.h
+++ Library.h
@@ -9,22 +9,24 @@
 #include <sstream>
 using namespace std;
 
-/***************************LIBRARYTREE PUBLIC CLASS OPERATIONS********************************
-	bool searchSpecific(title)				--> Searches for title using partial or full title.
-												Must begin with start of title
-												(Ex: Harry Potter, NOT arry Potter)
-	void searchAll(title)					--> Searches for all instances of a key word.
-												Must begin with start of title
-	void checkStatus(title)					--> Check status of book in database
-	void clearAll();						--> Clears tree
-	void printTree() const;					--> Prints tree inorder (by title)
-	void printTreeA() const;				--> Prints tree inorder (by author)
+/***************************LIBRARYTREE PUBLIC CLASS OPERATIONS********************************************
+	bool searchSpecific(title)			--> Searches for title using partial or full title.
+							Must begin with start of title
+							(Ex: Harry Potter, NOT arry Potter)
+	void searchAll(title)				--> Searches for all instances of a key word.
+							Must begin with start of title
+	void checkStatus(title)				--> Check status of book in database
+	void clearAll();				--> Clears tree
+	void printTree() const;				--> Prints tree inorder (by title)
+	void printTreeA() const;			--> Prints tree inorder (by author)
 	void changeBookStatus(title, int)		--> Changes book status of file and tree (0 or 1)
-/*********************************PUBLIC OPERATIONS********************************************
-	void insertBook(&tree, title, author)	--> Adds a new book to .csv file
+/*********************************PUBLIC OPERATIONS********************************************************
+	void insertBook(&tree, title, author)		--> Adds a new book to .csv file
 	void removeBook(&tree, title)			--> Removes book from .csv file
 	void fillTree(&tree, filename)			--> Fills initial tree with books from file
-/**********************************************************************************************/
+/**********************************************************************************************************/
+
+
 
 class Book
 {
